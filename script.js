36fbc5fa346f58e503861dbfa7d0a393369933c6
Logravia <j.alunans@gmail.com>
2021-12-29 23:36:45 +0200

    add functionality to color 'pixels' onmouseenter

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const canvas = document.querySelector('.canvas')
 
-
 const populateCanvas = (rows, columns) => {
 
+    // make rows for pixels to be in
     for (let i = 0; i < rows; i++) {
         let row = document.createElement('div');
         row.setAttribute("class", "row");
 
+        //make pixels in each row
         for (let j = 0; j < columns; j++) {
             let pixel = document.createElement('div');
             pixel.setAttribute("class", "pixel");
+
+            // color pixel when mouse enters it
+            pixel.addEventListener("mouseenter",colorPixel);
             row.appendChild(pixel);
         }
         canvas.appendChild(row);
     }
 }
 
-populateCanvas(8,8);
+const colorPixel = (e) => {
+    e.target.setAttribute("style", "background-color : black");
+}
+
+populateCanvas(4,4);
