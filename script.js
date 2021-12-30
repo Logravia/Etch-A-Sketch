e4cf184c4bf3d0bbf05dbc96f38571262f352d49
Logravia <j.alunans@gmail.com>
2021-12-30 11:51:50 +0200

    add rgb generator

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const canvas = document.querySelector('.canvas')
+const canvas = document.querySelector('.canvas');
+const buttons = document.querySelectorAll('button');
+const pixelTheme = "red";
 
 const populateCanvas = (rows, columns) => {
     // make rows for pixels to be in
@@ -11,16 +13,30 @@ const populateCanvas = (rows, columns) => {
             let pixel = document.createElement('div');
             pixel.setAttribute("class", "pixel");
 
-            // color pixel when mouse enters it
-            pixel.addEventListener("mouseenter",colorPixel);
+            // color pixel when mouse enters it with the defined theme
+        pixel.addEventListener("mouseenter",colorPixel);
             row.appendChild(pixel);
         }
         canvas.appendChild(row);
     }
 }
 
-const colorPixel = (e, color="black") => {
-    e.target.setAttribute("style", `background-color : ${color}`);
+const colorPixel = (e) => {
+    e.target.setAttribute("style", `background-color : ${rgbGenerator()}`);
 }
 
+const changePixelTheme = (e) => {
+    pixelTheme = e.currentTarget.id
+}
+
+const getRandInt = (max) => {
+    return Math.floor(Math.random() * max); 
+}
+
+const rgbGenerator = (limitR ='256', limitG='256', limitB='256') => {
+    return `rgb(${getRandInt(limitR)},${getRandInt(limitG)},${getRandInt(limitB)})`
+}
+
+buttons.forEach(button => button.addEventListener("click", changePixelTheme, false))
+        
 populateCanvas(16,16);
