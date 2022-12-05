951097d3f006dbf71ea0564ac0ab7d76d297ad89
Jānis Alunāns <j.alunans@gmail.com>
2022-12-05 12:25:53 +0200

    Add 3 more colors

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ let pixelTheme = 'autumn';
 
 const hslThemeSet = {
                     'rainbow' : {'minH': 0, 'maxH': 360, 'minS': 50, 'maxS': 100, 'minL': 30, 'maxL' : 90}, 
-                    'purple': {'minH': 270, 'maxH': 310, 'minS': 20, 'maxS': 90, 'minL': 30, 'maxL' : 55}, 
+                    'purple': {'minH': 270, 'maxH': 310, 'minS': 20, 'maxS': 90, 'minL': 30, 'maxL' : 55},
+                    'pink': {'minH': 298, 'maxH': 305, 'minS': 80, 'maxS': 100, 'minL': 50, 'maxL' : 100},
                     'volcano': {'minH': 345, 'maxH': 360, 'minS': 70, 'maxS': 90, 'minL': 15, 'maxL' : 50},
                     'winter': {'minH': 190, 'maxH': 230, 'minS': 50, 'maxS': 90, 'minL': 40, 'maxL' : 100},
                     'spring': {'minH': 100, 'maxH': 130, 'minS': 65, 'maxS': 95, 'minL': 40, 'maxL' : 85},
                     'summer': {'minH': 85, 'maxH': 130, 'minS': 85, 'maxS': 100, 'minL': 15, 'maxL' : 40},
                     'autumn': {'minH': 20, 'maxH': 100, 'minS': 85, 'maxS': 100, 'minL': 10, 'maxL' : 35},
+                    'grey': {'minH': 35, 'maxH': 40, 'minS': 0, 'maxS': 10, 'minL': 55, 'maxL' : 75},
+                    'darkazure': {'minH': 230, 'maxH': 240, 'minS': 0, 'maxS': 100, 'minL': 10, 'maxL' : 25},
+                    'darkblue': {'minH': 230, 'maxH': 240, 'minS': 15, 'maxS': 60, 'minL': 10, 'maxL' : 15},
                  };
 
 const populateCanvas = (rows, columns) => {
